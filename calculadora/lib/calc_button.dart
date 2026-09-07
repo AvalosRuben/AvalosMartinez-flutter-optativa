4396a57e5a8299e319549b7bcc18187e7f53b403
AvalosRuben <rubenavalosmtz@gmail.com>
2026-09-07 08:27:05 -0600

    feat(Tarea2): Añadi padding al boton

--- calculadora/lib/calc_button.dart
+++ calculadora/lib/calc_button.dart
@@ -7,6 +7,10 @@ class Calcbutton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(onPressed: () => {}, child: Text(numero));
+    return ElevatedButton(
+      onPressed: () => {},
+      style: ElevatedButton.styleFrom(padding: const EdgeInsets.all(20)),
+      child: Text(numero),
+    );
   }
 }
