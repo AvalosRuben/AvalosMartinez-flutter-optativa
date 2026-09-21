21b5db674cc7195101800c8f18d222768282f59d
AvalosRuben <rubenavalosmtz@gmail.com>
2026-09-21 12:43:37 -0600

    feat(Pantalla3): Pantalla 3 terminada

--- calculadora/lib/main.dart
+++ calculadora/lib/main.dart
@@ -22,51 +22,49 @@ class MainMenu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Menu'),
-          centerTitle: true,
-          backgroundColor: Colors.indigoAccent,
-          foregroundColor: Colors.white,
-          elevation: 1,
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              menuButton(
-                context,
-                label: "Calculadora",
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const Calculator()),
-                  );
-                },
-              ),
-              menuButton(
-                context,
-                label: "Pantalla 2",
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const Pantalla2()),
-                  );
-                },
-              ),
-              menuButton(
-                context,
-                label: "Pantalla 3",
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const Pantalla3()),
-                  );
-                },
-              ),
-            ],
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Menu'),
+        centerTitle: true,
+        backgroundColor: Colors.indigoAccent,
+        foregroundColor: Colors.white,
+        elevation: 1,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            menuButton(
+              context,
+              label: "Calculadora",
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Calculator()),
+                );
+              },
+            ),
+            menuButton(
+              context,
+              label: "Pantalla 2",
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Pantalla2()),
+                );
+              },
+            ),
+            menuButton(
+              context,
+              label: "Pantalla 3",
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Pantalla3()),
+                );
+              },
+            ),
+          ],
         ),
       ),
     );
--- calculadora/lib/pages/pantalla3.dart
+++ calculadora/lib/pages/pantalla3.dart
@@ -16,7 +16,42 @@ class Pantalla3 extends StatelessWidget {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [Text('Nombre del producto'), const SizedBox(height: 20)],
+          children: [
+            const Text(
+              'Suculenta',
+              style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 20),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(16),
+              child: Image.asset(
+                'assets/images/plantasuculenta.jpg',
+                width: 220,
+                height: 280,
+                fit: BoxFit.cover,
+              ),
+            ),
+            const SizedBox(height: 26),
+            const Text(
+              'Precio: 200 MXN',
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
+            ),
+            const SizedBox(height: 12),
+            const Text(
+              'Descripción',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 12),
+            const Text(
+              'Bonita planta suculenta, de fácil cuidado, regado 1 vez a la semana, incluye maceta blanca.',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 16,
+                height: 1.4, // Mejora el espaciado entre líneas
+                color: Colors.black87,
+              ),
+            ),
+          ],
         ),
       ),
     );
