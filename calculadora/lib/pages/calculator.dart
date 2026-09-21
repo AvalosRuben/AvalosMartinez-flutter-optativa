16b06348ce652ad9b8807012c8dbc65eab88f105
AvalosRuben <rubenavalosmtz@gmail.com>
2026-09-20 19:21:53 -0600

    feat(Menu): Borre un materialApp component innecesario

--- calculadora/lib/pages/calculator.dart
+++ calculadora/lib/pages/calculator.dart
@@ -96,104 +96,101 @@ class _CalculatorState extends State<Calculator> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      scaffoldMessengerKey: messengerKey,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Calculadora'),
-          centerTitle: true,
-          backgroundColor: Colors.indigoAccent,
-          foregroundColor: Colors.white,
-          elevation: 1,
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('Resultado:$_resultado'),
-              const SizedBox(height: 40),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Expanded(
-                    child: Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 8.0),
-                      child: TextField(
-                        controller: _num1Controller,
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'Numero1',
-                        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Calculadora'),
+        centerTitle: true,
+        backgroundColor: Colors.indigoAccent,
+        foregroundColor: Colors.white,
+        elevation: 1,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Resultado:$_resultado'),
+            const SizedBox(height: 40),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 8.0),
+                    child: TextField(
+                      controller: _num1Controller,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Numero1',
                       ),
                     ),
                   ),
-                  Expanded(
-                    child: Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 8.0),
-                      child: TextField(
-                        controller: _num2Controller,
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'Numero2',
-                        ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 8.0),
+                    child: TextField(
+                      controller: _num2Controller,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Numero2',
                       ),
                     ),
                   ),
-                ],
-              ),
-              const SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Calcbutton(numero: 1, onPress: () => _cambiarNumero(1)),
-                  Calcbutton(numero: 2, onPress: () => _cambiarNumero(2)),
-                  Calcbutton(numero: 3, onPress: () => _cambiarNumero(3)),
-                ],
-              ),
-              const SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Calcbutton(numero: 4, onPress: () => _cambiarNumero(4)),
-                  Calcbutton(numero: 5, onPress: () => _cambiarNumero(5)),
-                  Calcbutton(numero: 6, onPress: () => _cambiarNumero(6)),
-                ],
-              ),
-              const SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Calcbutton(numero: 7, onPress: () => _cambiarNumero(7)),
-                  Calcbutton(numero: 8, onPress: () => _cambiarNumero(8)),
-                  Calcbutton(numero: 9, onPress: () => _cambiarNumero(9)),
-                ],
-              ),
-              const SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ElevatedButton(onPressed: _sumar, child: const Text('SUMAR')),
-                  ElevatedButton(onPressed: _resta, child: const Text('RESTA')),
-                ],
-              ),
-              const SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ElevatedButton(
-                    onPressed: _multiplicacion,
-                    child: const Text('MULTIPLICACIÓN'),
-                  ),
-                  ElevatedButton(
-                    onPressed: _division,
-                    child: const Text('DIVISION'),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(onPressed: _borrar, child: Text('BORRAR')),
-            ],
-          ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Calcbutton(numero: 1, onPress: () => _cambiarNumero(1)),
+                Calcbutton(numero: 2, onPress: () => _cambiarNumero(2)),
+                Calcbutton(numero: 3, onPress: () => _cambiarNumero(3)),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Calcbutton(numero: 4, onPress: () => _cambiarNumero(4)),
+                Calcbutton(numero: 5, onPress: () => _cambiarNumero(5)),
+                Calcbutton(numero: 6, onPress: () => _cambiarNumero(6)),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Calcbutton(numero: 7, onPress: () => _cambiarNumero(7)),
+                Calcbutton(numero: 8, onPress: () => _cambiarNumero(8)),
+                Calcbutton(numero: 9, onPress: () => _cambiarNumero(9)),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(onPressed: _sumar, child: const Text('SUMAR')),
+                ElevatedButton(onPressed: _resta, child: const Text('RESTA')),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: _multiplicacion,
+                  child: const Text('MULTIPLICACIÓN'),
+                ),
+                ElevatedButton(
+                  onPressed: _division,
+                  child: const Text('DIVISION'),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(onPressed: _borrar, child: Text('BORRAR')),
+          ],
         ),
       ),
     );
